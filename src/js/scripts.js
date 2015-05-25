b1cc53c0256f7a7e4fc9a1f9dc3f12fc7993034b
Mika Väisänen <mika.vaisanen@reaktor.fi>
2015-05-25 11:15:41 +0300

    more mathjax config

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,11 +3,12 @@
 var textBox = document.querySelector('textarea');
 
 
-var outputBox = document.querySelector('.output');
+var outputBox = document.querySelector('#output');
 var input = textBox.value;
 
 textBox.addEventListener('input', function (event) {
 	input = textBox.value;
 	outputBox.textContent = input;
+	MathJax.Hub.Queue(['Typeset', MathJax.Hub, "output" ]);
 });
 
