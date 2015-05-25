5dbdc30532a757ae2bfd9105da57e243edd3b2bf
Mika Väisänen <mika.vaisanen@reaktor.fi>
2015-05-25 11:37:12 +0300

    fixed gulp scss compiler

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('bs-reload', function () {
 });
 
 gulp.task('default', ['css', 'js', 'browser-sync'], function () {
-	gulp.watch('src/scss/*/*.scss', ['css']);
+	gulp.watch('src/scss/*.scss', ['css']);
 	gulp.watch('src/js/*.js', ['js']);
 	gulp.watch('app/*.html', ['bs-reload']);
 });
