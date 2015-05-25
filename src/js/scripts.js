717fef559355a10cb7d7d74cdd3d10cd5fea1e34
Mika Väisänen <mika.vaisanen@reaktor.fi>
2015-05-25 10:57:06 +0300

    textbox, input and output

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var textBox = document.querySelector('textarea');
+
+
+var outputBox = document.querySelector('.output');
+var input = textBox.value;
+
+textBox.addEventListener('input', function (event) {
+	input = textBox.value;
+	outputBox.textContent = input;
+});
+
