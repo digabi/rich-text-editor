ec823617b6f00b4a38c5dd99bd601f40031dadd5
Mika Väisänen <mika.vaisanen@reaktor.fi>
2015-05-26 10:39:24 +0300

    tabs and editor

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,29 @@
 'use strict';
 
-var textBox = document.querySelector('textarea');
-
-
-var outputBox = document.querySelector('#output');
-var input = textBox.value;
-
-textBox.addEventListener('input', function (event) {
-	input = textBox.value;
-	outputBox.textContent = '`' + input + '`';
-	MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'output' ]);
+$(document).ready(function() {
+	var textBox = $('.mathquill-editable');
+	// $(document).on('click', function () {
+	// 	textBox.mathquill('write','\\frac{d}{dx}');
+	// });
+	$('.syntax-tab > div').on('click', function (event) {
+		var syntax = ($(event.currentTarget).attr('id'));
+		textBox.mathquill('write', syntax);
+	});
 });
+
+
+
+// var textBox = document.querySelector('textarea');
+
+
+// var outputBox = document.querySelector('#output');
+// var input = textBox.value;
+
+// textBox.addEventListener('input', function (event) {
+// 	input = textBox.value;
+// 	outputBox.textContent = '`' + input + '`';
+// 	MathJax.Hub.Queue(['Typeset', MathJax.Hub, 'output' ]);
+// });
+
+
 
