f2a7d03d8154f542f40fa1e7b894fcfaa020cb9b
Eero Anttila <eea@iki.fi>
2017-03-17 11:43:44 +0200

    Introduce express

---
+++ app/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const express = require('express')
+const port = process.env.PORT || 5000
+const browserify = require('browserify-middleware')
+const app = express()
+app.use('/front.min.js', browserify(__dirname + '/front.js'))
+app.use(express.static(__dirname + '/../public'))
+app.use('/bootstrap', express.static(__dirname + '/../node_modules/bootstrap'))
+app.use('/jquery', express.static(__dirname + '/../node_modules/jquery'))
+app.use('/mathquill', express.static(__dirname + '/../node_modules/mathquill'))
+app.use('/mathjax', express.static(__dirname + '/../node_modules/mathjax'))
+app.listen(port, () => console.log('Server started at localhost:' + port))
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-npm install -g bower
-npm install -g gulp
-npm install
-bower install
-gulp
