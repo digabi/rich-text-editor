48a094f1622f63fe0c1ac81ced77bb6ec74827c4
Eero Anttila <eea@iki.fi>
2017-03-17 15:59:02 +0200

    Remove unncecessary ready callback

--- app/front.js
+++ app/front.js
@@ -1,99 +1,97 @@
 'use strict';
 
-$(document).ready(function() {
-	const tabs = $('.nav-tabs')
-	const tabContent = $('.tab-content')
-	tabs.find('a').click(e => {
-		tabs.find('.active').removeClass('active')
-		e.target.parentNode.classList.add('active')
-		tabContent.find('.active').removeClass('active')
-		tabContent.find(e.target.getAttribute('href')).addClass('active')
-		e.preventDefault()
-		return false
-	})
-	var latexMath = $('#editable-math'),
-		latexSource = $('#latex-source'),
-		svgOutput = $('#svg-output'),
-		typingTimer = null,
-		doneTypingInterval = 500,
-		container = $('.container'),
-		output = $('.output')
+const tabs = $('.nav-tabs')
+const tabContent = $('.tab-content')
+tabs.find('a').click(e => {
+	tabs.find('.active').removeClass('active')
+	e.target.parentNode.classList.add('active')
+	tabContent.find('.active').removeClass('active')
+	tabContent.find(e.target.getAttribute('href')).addClass('active')
+	e.preventDefault()
+	return false
+})
+var latexMath = $('#editable-math'),
+	latexSource = $('#latex-source'),
+	svgOutput = $('#svg-output'),
+	typingTimer = null,
+	doneTypingInterval = 500,
+	container = $('.container'),
+	output = $('.output')
 
-	latexSource.focus(onFocus)
-	var children = latexMath.find('.textarea').children()
-	children.focus(onFocus)
-	latexSource.blur(onBlur)
-	children.blur(onBlur)
+latexSource.focus(onFocus)
+var children = latexMath.find('.textarea').children()
+children.focus(onFocus)
+latexSource.blur(onBlur)
+children.blur(onBlur)
 
-	output.click(onFocus)
-	function onFocus() {
-		container.addClass('focus')
-	}
+output.click(onFocus)
+function onFocus() {
+	container.addClass('focus')
+}
 
-	function onBlur() {
-		container.removeClass('focus')
-	}
+function onBlur() {
+	container.removeClass('focus')
+}
 
-	function latexMathToLatexSource() {
-		setTimeout(function() {
-			var latex = latexMath.mathquill('latex');
-			latexSource.val(latex);
-		}, 0);
-	}
+function latexMathToLatexSource() {
+	setTimeout(function() {
+		var latex = latexMath.mathquill('latex');
+		latexSource.val(latex);
+	}, 0);
+}
 
-	function latexSourceToLatexMath() {
-		var oldtext = latexSource.val();
-		setTimeout(function() {
-			var newtext = latexSource.val();
-			if(newtext !== oldtext) {
-				latexMath.mathquill('latex', newtext);
-			}
-		}, 0);
-	}
+function latexSourceToLatexMath() {
+	var oldtext = latexSource.val();
+	setTimeout(function() {
+		var newtext = latexSource.val();
+		if(newtext !== oldtext) {
+			latexMath.mathquill('latex', newtext);
+		}
+	}, 0);
+}
 
-	function render() {
-		clearTimeout(typingTimer);
-		typingTimer = setTimeout(function() {
-			updateMath(latexSource.val());
-		}, doneTypingInterval);
-	}
+function render() {
+	clearTimeout(typingTimer);
+	typingTimer = setTimeout(function() {
+		updateMath(latexSource.val());
+	}, doneTypingInterval);
+}
 
-	var updateMath = (function() {
+var updateMath = (function() {
 
-		var queue = MathJax.Hub.queue,
-			math = null,
-			box = null,
+	var queue = MathJax.Hub.queue,
+		math = null,
+		box = null,
 
-			hideBox = function() {
-				box.style.visibility = 'hidden';
-			},
-			showBox = function() {
-				box.style.visibility = 'visible';
-			};
-
-		queue.Push(function() {
-			math = MathJax.Hub.getAllJax('MathOutput')[0];
-			box = document.getElementById('box');
-			showBox();
-		});
-
-		return function(latex) {
-			queue.Push(hideBox, ['Text', math, '\\displaystyle{' + latex + '}'],
-				showBox);
+		hideBox = function() {
+			box.style.visibility = 'hidden';
+		},
+		showBox = function() {
+			box.style.visibility = 'visible';
 		};
 
-	})();
-
-	$('.syntax-tab > div').on('click', function(event) {
-		var syntax = ($(event.currentTarget).attr('id'));
-		latexMath.mathquill('write', syntax);
-		latexMathToLatexSource();
-		render();
+	queue.Push(function() {
+		math = MathJax.Hub.getAllJax('MathOutput')[0];
+		box = document.getElementById('box');
+		showBox();
 	});
 
-	$('#latex-source, #editable-math').on('keydown', render);
+	return function(latex) {
+		queue.Push(hideBox, ['Text', math, '\\displaystyle{' + latex + '}'],
+			showBox);
+	};
 
-	latexMath.bind('keydown keypress', latexMathToLatexSource).keydown().focus();
-	latexSource.bind('keydown keypress', latexSourceToLatexMath);
+})();
 
+$('.syntax-tab > div').on('click', function(event) {
+	var syntax = ($(event.currentTarget).attr('id'));
+	latexMath.mathquill('write', syntax);
+	latexMathToLatexSource();
+	render();
 });
+
+$('#latex-source, #editable-math').on('keydown', render);
+
+latexMath.bind('keydown keypress', latexMathToLatexSource).keydown().focus();
+latexSource.bind('keydown keypress', latexSourceToLatexMath);
+
