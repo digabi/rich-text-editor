97bfb29ed8daad736a7340e27865f95fbd20701c
Eero Anttila <eea@iki.fi>
2017-03-23 16:07:50 +0200

    Rename files

--- app/front.js
+++ app/front.js
@@ -1,83 +1,125 @@
-initTabs()
+const equationEditor = document.querySelector('.equationEditor')
+const latexEditor = document.querySelector('.latexEditor')
+const resultNode = document.querySelector('.result')
+const MQ = MathQuill.getInterface(2)
 
-const visualMath = $('#editable-math')
-const latexSource = $('#latex-source')
-const doneTypingInterval = 500
-const container = $('.container')
-const output = $('.output')
-let typingTimer = null
+initToolbar()
+$('.mathToolbar').hide()
 
-const children = visualMath.find('.textarea').children()
-$(latexSource, children).focus(onFocus).blur(onBlur)
-output.click(onFocus)
-
-function onFocus() { container.addClass('focus') }
-function onBlur() { container.removeClass('focus') }
-
-$('.syntax-tab > div').on('click', event => {
-	const syntax = ($(event.currentTarget).attr('id'))
-	visualMath.mathquill('write', syntax)
-	visualToLatex()
-	throttledEquationUpdate()
+$(resultNode).click(() => {
+	$('.math').addClass('focus')
+	$('.mathToolbar').show()
+	mathField.reflow()
+	mathField.focus()
 })
 
-$(visualMath, latexSource).on('keydown', throttledEquationUpdate)
-
-visualMath.bind('keydown keypress', visualToLatex).keydown().focus()
-latexSource.bind('keydown keypress', latexToVisual)
-
-function visualToLatex() {
-	setTimeout(() => {
-		const latex = visualMath.mathquill('latex')
-		latexSource.val(latex)
-	}, 0)
-}
-
-function latexToVisual() {
-	const oldtext = latexSource.val()
-	setTimeout(() => {
-		const newtext = latexSource.val()
-		if(newtext !== oldtext) {
-			visualMath.mathquill('latex', newtext)
-		}
-	}, 0)
-}
-
-function throttledEquationUpdate() {
-	clearTimeout(typingTimer)
-	typingTimer = setTimeout(() => {
-		equationUpdate(latexSource.val())
-	}, doneTypingInterval)
-}
-
-const equationUpdate = (() => {
-	const queue = MathJax.Hub.queue
-	let math = null
-	let box = null
-
-	const hideBox = () => box.style.visibility = 'hidden'
-	const showBox = () => box.style.visibility = 'visible'
-
-	queue.Push(() => {
-		math = MathJax.Hub.getAllJax('MathOutput')[0]
-		box = document.getElementById('box')
-		showBox()
-	})
-
-	return latex => {
-		queue.Push(hideBox, ['Text', math, '\\displaystyle{' + latex + '}'], showBox)
+$('.editMode .close').click(e => {
+	e.preventDefault()
+	$('.math').removeClass('focus')
+	$('.mathToolbar').hide()
+	updateResult()
+})
+const mathField = MQ.MathField(equationEditor, {
+	spaceBehavesLikeTab: true,
+	handlers:            {
+		edit: () => latexEditor.value = mathField.latex()
 	}
-})()
+});
+latexEditor.addEventListener('keyup', () => {
+	setTimeout(() => mathField.latex(latexEditor.value), 0)
+})
+mathField.latex(latexEditor.value)
 
-function initTabs() {
-	const tabs = $('.nav-tabs')
-	const tabContent = $('.tab-content')
-	tabs.find('a').click(e => {
-		tabs.find('.active').removeClass('active')
-		e.target.parentNode.classList.add('active')
-		tabContent.find('.active').removeClass('active')
-		tabContent.find(e.target.getAttribute('href')).addClass('active')
+$('.answer').get(0).focus()
+
+
+function updateResult() {
+	$('img.result').prop('src', '/math.svg?latex=' +  latexEditor.value)
+}
+updateResult()
+
+function initToolbar() {
+	const actions = [
+		{action: '\\sqrt', label: '\\sqrt{\\square}'},
+		{action: '^', label: '\\square^{\\square}'},
+		{action: '\\frac', label: '\\frac{\\square}{\\square}'},
+		{action: '\\int', label: '\\int_{\\square}^{\\square}'},
+		{action: '\\neq', label: '\\neq'},
+		{action: '\\lim_', label: '\\lim_{\\square}'},
+		{action: '\\to', label: '\\to'},
+		{action: '\\overrightarrow', label: '\\overrightarrow{\\square}'}
+	]
+
+	const tags = [
+		{action: '<sub>x</sub>', label: 'X<sub>alaindeksi</sub>'},
+		{action: '<sup>x</sup>', label: 'X<sup>yläindeksi</sup>'},
+		{action: '<i>italic</i>', label: '<i>Kursiivi</i>'}
+	]
+	$('.tags .list').append(tags.map(o => $(`<button id="${o.action}">${o.label}</button>`)))
+	$('.mathToolbar').append(actions.map(o => $(`<button id="${o.action}">${o.label}</button>`)))
+	const buttons = $('.mathToolbar button')
+	buttons.mousedown(e => {
 		e.preventDefault()
-		return false
+		const symbol = e.currentTarget.id
+		mathField.typedText(symbol)
+		if(symbol.startsWith('\\')) mathField.keystroke('Tab')
+		setTimeout(() => mathField.focus(), 0)
 	})
+	return buttons.map((i, elem) => MQ.StaticMath(elem))
+}
+const chars = [
+	'°', '≡',
+	'⌐', '×', '«', '»', '…', '∫', '¬', '√', 'ƒ', '≈', '‹', '›', '∙', '‰', '¹', '²', '³', '½', '¼', '¾', '←', '↑', '→', '↓', '↔', '↕', '↨', '≠', 'Ø', '∞', '±', '≤', '≥', 'µ', '∂', '∑', '∏', 'Ω', '∆', 'ı', 'Ð',
+	'ð', 'Þ', 'þ', 'Γ', 'Θ', 'Φ', 'α', 'δ', 'ε', 'σ', 'τ', 'φ', '∩', 'Ω', 'ω', 'Д', 'Ф'
+]
+
+const $characters = $('.toolbar .characters')
+
+$characters.find('.list').append(chars.map(char => $(`<span class="button">${char}</span>`)))
+$('.toolbar .button').mousedown(e => {
+	const innerText = e.currentTarget.innerText
+	if($('.equationEditor').hasClass('mq-focused')) {
+		mathField.typedText(innerText)
+	} else {
+		pasteHtmlAtCaret(innerText)
+	}
+	e.preventDefault()
+	return false
+})
+
+$('.tags button').mousedown(e => {
+	pasteHtmlAtCaret(e.currentTarget.id)
+	e.preventDefault()
+	return false
+})
+$('.toggle').mousedown(e => {
+	$(e.target.parentNode).find('.list').toggle()
+	e.preventDefault()
+	return false
+})
+
+function pasteHtmlAtCaret(html) {
+	let sel;
+	let range;
+	if(window.getSelection) {
+		sel = window.getSelection()
+		if(sel.getRangeAt && sel.rangeCount && sel.anchorNode.parentElement.classList.contains('answer')) {
+			range = sel.getRangeAt(0)
+			range.deleteContents()
+			const el = document.createElement("div")
+			el.innerHTML = html
+			let frag = document.createDocumentFragment(), node, lastNode
+			while((node = el.firstChild)) {
+				lastNode = frag.appendChild(node)
+			}
+			range.insertNode(frag)
+			if(lastNode) {
+				range = range.cloneRange()
+				range.setStartAfter(lastNode)
+				range.collapse(true)
+				sel.removeAllRanges()
+				sel.addRange(range)
+			}
+		}
+	}
 }
