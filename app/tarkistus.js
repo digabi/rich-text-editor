c891e5a714ef015bb6dbb187557acc867e32d2bf
Eero Anttila <eea@iki.fi>
2017-03-28 12:51:18 +0300

    Simplify code

--- app/tarkistus.js
+++ app/tarkistus.js
@@ -1,10 +1,11 @@
 let markers = {}
 let timestamp = null
+const $answer = $('.answer')
 $.get('/load', data => {
     if(!data)
         return
     timestamp = data.timestamp
-    $('.answer').html(data.html)
+    $answer.html(data.html)
         .find('.result')
         .each((i, elem) => $(elem).prop('id', i))
         .wrap('<div class="resultWrapper">')
@@ -12,25 +13,23 @@ $.get('/load', data => {
         if(data.timestamp === timestamp) {
             Object.keys(data.markers).forEach(key => {
                 data.markers[key].forEach(marker => {
-                    addMarker($('.answer').find('#' + key), marker)
+                    addMarker($answer.find('#' + key), marker)
                 })
             })
         }
     })
 })
 
-$('.answer').on('mousedown', '.result', e => {
-    const x = e.offsetX
-    const y = e.offsetY
-    addMarker(e.target, {x, y})
+$answer.on('mousedown', '.result', e => {
+    const left = e.offsetX
+    const top = e.offsetY
+    addMarker($(e.target), {left, top})
 })
 
-function addMarker(img, pos) {
-    const index = $(img).prop('id')
+function addMarker($img, pos) {
+    const index = $img.prop('id')
     markers[index] = (markers[index] || []).concat(pos)
-    const $marker = $('<div class="marker">')
-    $marker.css({left: pos.x, top: pos.y})
-    $(img).after($marker)
+    $img.after($('<div class="marker">').css(pos))
 }
 $('.save').click(() => {
     $.ajax({
