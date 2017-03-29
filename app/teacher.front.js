4fc1e1db4568fa1cf33059d5e531eb371532e8b8
Eero Anttila <eea@iki.fi>
2017-03-29 22:10:06 +0300

    Add possibility to underline images

--- app/teacher.front.js
+++ app/teacher.front.js
@@ -19,22 +19,66 @@ $.when(
         return
     Object.keys(markers.markers).forEach(key => {
         markers.markers[key].forEach(marker => {
-            addMarker($answer.find('#' + key), marker)
+            restoreRectangle($answer.find('#' + key), marker)
         })
     })
 })
 
+let mouseDownPos = null
+let $rectangle = null
 $answer.on('mousedown', '.result', e => {
-    const left = e.offsetX
-    const top = e.offsetY
-    addMarker($(e.target), {left, top})
+    e.preventDefault()
+    mouseDownPos = point(e)
+    $rectangle = $('<div class="rectangle">')
+    $(e.target).after($rectangle)
+})
+$answer.on('mousemove', '.result', e => {
+    e.preventDefault()
+    if (e.buttons === 0) {
+        if ($rectangle) onEnd(e)
+    } else {
+        setPos($rectangle, mouseDownPos, point(e))
+    }
 })
 
-function addMarker($img, pos) {
+function onEnd(e) {
+    savePos($(e.target), toCss(mouseDownPos, point(e)))
+    $rectangle = null
+}
+$answer.on('mouseup', '.result', e => {
+    e.preventDefault()
+    onEnd(e)
+})
+
+function point(e) {
+    return {
+        left: e.offsetX,
+        top:  e.offsetY
+    }
+}
+function restoreRectangle($img, pos) {
+    const $rectangle = $('<div class="rectangle">').css(pos)
+    $img.after($rectangle)
+    savePos($img, pos)
+}
+function savePos($img, pos) {
     const index = $img.prop('id')
     markers[index] = (markers[index] || []).concat(pos)
-    $img.after($('<div class="marker">').css(pos))
 }
+
+function toCss(pos1, pos2) {
+    return {
+        left:   Math.min(pos1.left, pos2.left),
+        top:    pos1.top,
+        width:  Math.abs(pos2.left - pos1.left),
+        height: 3
+    }
+}
+
+function setPos($img, pos1, pos2) {
+    $img.css(toCss(pos1, pos2))
+}
+
 $('.save').click(() => {
     $.ajax({
         type:        'POST',
