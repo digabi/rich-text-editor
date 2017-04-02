d7322068f482930c67966966a04ad1754782124f
Eero Anttila <eea@iki.fi>
2017-04-02 09:56:25 +0300

    Cleanup code

--- app/teacher.front.js
+++ app/teacher.front.js
@@ -4,8 +4,7 @@ const $answer = $('.answer')
 $.when(
     $.get('/load'),
     $.get('/loadMarkers')
-).done((load, loadMarkers) => {
-    const answer = load[0]
+).done(([answer], [markers]) => {
     if (!answer)
         return
     timestamp = answer.timestamp
@@ -14,7 +13,6 @@ $.when(
         .each((i, elem) => $(elem).prop('id', i))
         .wrap('<div class="resultWrapper">')
 
-    const markers = loadMarkers[0]
     if (markers.timestamp !== timestamp)
         return
     Object.keys(markers.markers).forEach(key => {
@@ -31,24 +29,32 @@ $answer.on('mousedown', '.result', e => {
     mouseDownPos = point(e)
     $rectangle = $('<div class="rectangle">')
     $(e.target).after($rectangle)
-})
-$answer.on('mousemove', '.result', e => {
+}).on('mousemove', '.result', e => {
     e.preventDefault()
     if (e.buttons === 0) {
         if ($rectangle) onEnd(e)
     } else {
         setPos($rectangle, mouseDownPos, point(e))
     }
+}).on('mouseup', '.result', e => {
+    e.preventDefault()
+    onEnd(e)
+})
+
+$('.save').click(() => {
+    $.ajax({
+        type:        'POST',
+        url:         '/saveMarkers',
+        data:        JSON.stringify({timestamp, markers}),
+        dataType:    'json',
+        contentType: 'application/json; charset=utf-8'
+    })
 })
 
 function onEnd(e) {
     savePos($(e.target), toCss(mouseDownPos, point(e)))
     $rectangle = null
 }
-$answer.on('mouseup', '.result', e => {
-    e.preventDefault()
-    onEnd(e)
-})
 
 function point(e) {
     return {
@@ -78,14 +84,3 @@ function toCss(pos1, pos2) {
 function setPos($img, pos1, pos2) {
     $img.css(toCss(pos1, pos2))
 }
-
-$('.save').click(() => {
-    $.ajax({
-        type:        'POST',
-        url:         '/saveMarkers',
-        data:        JSON.stringify({timestamp, markers}),
-        dataType:    'json',
-        contentType: 'application/json; charset=utf-8'
-    })
-})
-
