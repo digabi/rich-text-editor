78bd077e5e9eb6e67a6252dc7cde31f51e9a2fba
Eero Anttila <eea@iki.fi>
2017-04-05 10:27:13 +0300

    Update instructions for sv

--- app/SV.js
+++ app/SV.js
@@ -6,10 +6,15 @@ module.exports = {
 Det går att konstruera formler genom att klicka på beteckningarna i menyerna och/eller genom att skriva LaTeX.
 Det går förutom att skriva text och formler, att också att lägga till bilder i svarsfältet.`,
         shortcutTitle: `Tips på tangentkombinationer`,
-        shortcuts: `Ctrl-V                Lägg till en bild från klippbordet
-Ctrl-L / Ctrl-I       Skriv en formel
-Bråkstreck får man även med / -märket
-Multiplikationstecken får man även med * -märket`,
+        shortcuts: `<table><tbody>
+<tr><th>Lägg till en bild från klippbordet</th><td>Ctrl-V</td></tr>
+<tr><th>Skriv en formel</th><td>Ctrl-L tai Ctrl-I</td></tr>
+<tr><th colspan="2"></th></tr>
+<tr><th>Bråkstreck</th><td>/</td></tr>
+<tr><th>Multiplikationstecken</th><td>*</td></tr>
+<tr><th>Stäng</th><td>Ctrl-Enter</td></tr>
+</tbody>
+</table>`,
         formatting: 'Muotoilu',
         specialCharacters: 'Specialtecken',
         insertEquation: 'Lägg till formel',
