5abb5c67e7db4766e0bd5af94aff29503ae531ca
Eero Anttila <eea@iki.fi>
2017-04-11 23:02:42 +0300

    Cache toolbar latex

--- app/mathImg.js
+++ app/mathImg.js
@@ -1,26 +1,45 @@
 const mjAPI = require("mathjax-node")
+const latexCommands = require('./latexCommands')
 
 module.exports = {handler}
 
 mjAPI.config({MathJax: {}})
 mjAPI.start()
 
+const cache = {}
+latexCommands.map(o => o.label ? o.label.replace(/X/g, '\\square') : o.action)
+    .forEach(latex => {
+        mjAPI.typeset({
+            math: latex,
+            format: "TeX", // "inline-TeX", "MathML"
+            mml: false,
+            svg: true,
+        }, function (data) {
+            cache[latex] = data.svg
+        })
+    })
+
 function handler(req, res) {
-    mjAPI.typeset({
-        math: req.query.latex,
-        format: "TeX", // "inline-TeX", "MathML"
-        mml: false,
-        svg: true,
-    }, function (data) {
-        if (data.errors) {
-            res.type('svg')
-            res.send(`<svg xmlns="http://www.w3.org/2000/svg"  
+    res.type('svg')
+
+    if(req.query.latex in cache) {
+        console.log('cache')
+        res.send(cache[req.query.latex])
+    } else {
+        mjAPI.typeset({
+            math: req.query.latex,
+            format: "TeX", // "inline-TeX", "MathML"
+            mml: false,
+            svg: true,
+        }, function (data) {
+            if (data.errors) {
+                res.send(`<svg xmlns="http://www.w3.org/2000/svg"  
      xmlns:xlink="http://www.w3.org/1999/xlink">
     <text x="10" y="15" fill="red">${data.errors.join('<br>')}</text>
 </svg>`)
-        } else {
-            res.type('svg')
-            res.send(data.svg)
-        }
-    })
+            } else {
+                res.send(data.svg)
+            }
+        })
+    }
 }
