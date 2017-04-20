f9d5f875cbfdb773bae5d1e8448d9c329fef4ee9
Eero Anttila <eea@iki.fi>
2017-04-20 10:23:33 +0300

    Get rid of github banner

--- app/student.html.js
+++ app/student.html.js
@@ -19,13 +19,6 @@ module.exports = (obj) => { with(obj) return `
 <body>
 <article>
     <section>
-        <a href="https://github.com/digabi/math-editor"><img style="position: absolute; top: 0; right: 0; border: 0;"
-                                                             src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67"
-                                                             alt="Fork me on GitHub"
-                                                             data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
-        <script>
-
-        </script>
         <h1>${title}</h1>
         <p>${description}</p>
         <h2>${shortcutTitle}</h2>
@@ -43,6 +36,9 @@ module.exports = (obj) => { with(obj) return `
     <section>
         <div class="paragraph">
             ${updated} ${startedAt}
+        </div>
+        <div class="paragraph">
+            <a href="https://github.com/digabi/math-editor">GitHub</a>
         </div>
         <div class="paragraph">
             <a href="mailto:abitti@ylioppilastutkinto.fi?subject=Palaute / Math-editor">${sendFeedback}
