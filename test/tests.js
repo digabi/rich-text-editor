316db94cc2b6596872f40c50b07ee2ec0c15268c
Eero Anttila <eea@iki.fi>
2017-05-02 12:50:23 +0300

    Fix tests

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,17 @@
 describe('math editor', () => {
 
-    before(waitUntil(() => $('[data-js="tools"]').length > 0 &&  $('[data-js="tools"]').is(':hidden')))
+    before('wait for tools to hide', waitUntil(() => $('[data-js="tools"]').length > 0 &&  $('[data-js="tools"]').position().top < 0 ))
 
     describe('init', () => {
         it('answer field is contenteditable', () => {
             const $answer = $('[data-js="answer"]')
             expect($answer).to.have.attr('contenteditable', 'true')
         })
-        it('tools are hidden', () => expect($('.rich-text-editor-tools')).to.be.hidden)
     })
 
     describe('focus rich text', () => {
         before(() => $('.answer1').focus())
-        before(waitUntil(() => $('[data-js="tools"]').is(':visible')))
+        before('wait for tools visible', waitUntil(() => $('[data-js="tools"]').is(':visible')))
 
         it('shows tools', () => expect($('[data-js="charactersList"]')).to.be.visible)
         it('hide math tools', () => expect($('[data-js="mathToolbar"]')).to.be.hidden)
@@ -27,9 +26,12 @@ describe('math editor', () => {
 
     describe('keeps both fiels in sync', () => {
         before(() => $('[data-js="latexField"]').focus().val('x+y').keyup())
+        before(() => $('.answer1').focus())
         before(done => setTimeout(done, 0))
 
-        it('shows math in latex field', () => expect($('[data-js="equationField"]')).to.have.text('x+y​'))
+        it('shows math in latex field', () => {
+            expect($('[data-js="equationField"]')).to.have.text('x+y')
+        })
         it('shows math in img', () => expect($('img:first')).to.have.attr('src','/math.svg?latex=x%2By'))
     })
 })
