0b788738206b5d2e39e476472668d7cb0ad1b861
Eero Anttila <eea@iki.fi>
2017-05-08 13:49:10 +0300

    Test input to equation field

--- test/tests.js
+++ test/tests.js
@@ -64,15 +64,27 @@ describe('math editor', () => {
             it('shows math tools', () => expect($('[data-js="mathToolbar"]')).to.be.visible)
             it('shows math editor', () => expect($('[data-js="mathEditor"]')).to.be.visible)
 
-            describe('keeps both fiels in sync', () => {
-                before(() => $('[data-js="latexField"]').focus().val('x+y').keyup())
-                before(() => $answer1.focus())
-                before(done => setTimeout(done, 0))
+            describe('keeps equation field in sync', () => {
+                before(() => $('[data-js="latexField"]').focus().val('x+y').trigger('input'))
+                before(done => setTimeout(done, 810))
 
-                it('shows math in latex field', () => {
-                    expect($('[data-js="equationField"]')).to.have.text('x+y')
+                after(() => $('[data-js="latexField"]').val('').trigger('input'))
+                after(done => setTimeout(done, 0))
+
+                it('shows math in equation field', () => {
+                    expect($('[data-js="equationField"]')).to.have.text('x+y​')
                 })
                 it('shows math in img', () => expect($('img:first')).to.have.attr('src', '/math.svg?latex=x%2By'))
+            })
+
+            describe('keeps latex field in sync', () => {
+                before(() => $('[data-js="equationField"] textarea').val('a+b').trigger('paste'))
+                before(done => setTimeout(done, 810))
+
+                it('shows math in latex field', () => {
+                    expect($('[data-js="latexField"]')).to.have.value('a+b')
+                })
+                it('shows math in img', () => expect($('img:first')).to.have.attr('src', '/math.svg?latex=a%2Bb'))
             })
         })
     })
