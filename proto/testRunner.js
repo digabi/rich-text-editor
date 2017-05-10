5d1803c507b838f83d60061abe1507eb51a17997
Eero Anttila <eea@iki.fi>
2017-05-10 15:38:10 +0300

    Output test results asynchronously

--- proto/testRunner.js
+++ proto/testRunner.js
@@ -2,14 +2,14 @@
 const server = require('./server')
 const serverInfo = server.listen(0, () => {
     const spawn = require('child_process').spawn
-    const ls = spawn('mocha-phantomjs',  [`http://localhost:${serverInfo.address().port}/tests.html`])
-    let response = []
-    ls.stdout.on('data', (data) => {
-        response.push(data.toString())
-    })
-    ls.on('close', code => {
-        console.log(response.join(''))
+    const mochaPhantomjs = spawn('mocha-phantomjs',  [`http://localhost:${serverInfo.address().port}/tests.html`])
+    mochaPhantomjs.stdout.pipe(process.stdout)
+    mochaPhantomjs.on('close', code => {
         process.exit(code)
+    })
+    mochaPhantomjs.on('error', err => {
+        console.log(err)
+        process.exit(1)
     })
     console.log('Server started at localhost:' + serverInfo.address().port)
 })
