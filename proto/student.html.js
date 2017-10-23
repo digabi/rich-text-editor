7fe0f66c201b194ef85a9e082e447568d252e98a
Eero Anttila <eea@iki.fi>
2017-10-23 16:19:56 +0300

    Display only one answer field and don't save images to server side

--- proto/student.html.js
+++ proto/student.html.js
@@ -32,9 +32,6 @@ module.exports = (obj) => { with(obj) return `
 
         <h2>${answerTitle} 1</h2>
         <div class="answer" id="answer1"></div>
-        <h2>${answerTitle} 2</h2>
-        <div class="answer" id="answer2"></div>
-        <button class="save actionButton" style="display: none">${save}</button>
     </section>
 </article>
 <footer>
@@ -54,49 +51,29 @@ module.exports = (obj) => { with(obj) return `
         </div>
     </section>
 </footer>
-<script src="/saver.js"></script>
 <script src="/rich-text-editor-bundle.js"></script>
 <script>
-    const $answer = $('.answer')
-    const save = ($elem, async = true) => $.post({
-        url: '/save',
-        data: {text: $elem.html(), answerId: $elem.attr('id')},
-        async
-    })
-
-    const richTextOptions = id => ({
+    const answer = document.getElementById('answer1')
+    makeRichText(answer, {
         screenshot: {
-            saver: data => saveScreenshot(id)(data),
+            saver: ({data}) =>
+                new Promise(resolve => {
+                    const reader = new FileReader()
+                    reader.onload = evt => resolve(evt.target.result)
+                    reader.readAsDataURL(data)
+                }),
             limit: 10
-        }
+        },
+        baseUrl: 'https://math-demo.abitti.fi'
     })
-
-    $answer.each((i, answer) => {
-        $.get('/load?answerId=' + answer.id, data => {
-            !window.IS_TEST && data && $(answer).html(data.html)
-            makeRichText(answer, richTextOptions(answer.id), onValueChange)
-        })
-    }).on('keypress', e => {
-        if (e.ctrlKey && !e.altKey && !e.shiftKey && e.key === 's') {
-            e.preventDefault()
-            save($(e.target))
-        }
-    })
-    $('#answer1').focus()
-
-    $('.toggleInstructions').click(e => {
-        e.preventDefault()
-        $('.instructions').toggleClass('hide')
-    })
-    function onValueChange() {
-    }
+    answer.focus()
 </script>
 <script>
     (function (i, s, o, g, r, a, m) {
         i['GoogleAnalyticsObject'] = r;
         i[r] = i[r] || function () {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
         a = s.createElement(o),
             m = s.getElementsByTagName(o)[0];
         a.async = 1;
