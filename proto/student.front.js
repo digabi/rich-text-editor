88c724def12062184fee1f0a736ea8acbe518aa0
Eero Anttila <eea@iki.fi>
2018-02-16 10:23:31 +0200

    Send detailed analytics information

--- proto/student.front.js
+++ proto/student.front.js
@@ -28,6 +28,46 @@ if (window.addEventListener) {
     window.onerror = trackError
 }
 const $tools = $('[data-js="tools"]')
-$tools.on('mousedown', '[data-js="expandCollapseCharacters"]', () => ga('send', 'event', 'toolbar', 'toggle', ($tools.hasClass('rich-text-editor-characters-expanded') ? 'expand' : 'collapse')))
-$('[data-js="mathToolbar"]').on('mousedown', 'button', e => ga('send', 'event', 'toolbar', 'latex', e.currentTarget.dataset.latexcommand))
-$('[data-js="charactersList"]').on('mousedown', 'button', e => ga('send', 'event', 'toolbar', 'character', e.currentTarget.innerText))
+const events = {
+    typeLatex: 0,
+    typeMathquill: 0,
+    clickChar: 0,
+    clickLatex: 0
+}
+let hasEvents = false
+$tools.on('mousedown', '[data-js="expandCollapseCharacters"]', () => {
+    ga('send', 'event', 'toolbar', 'toggle', ($tools.hasClass('rich-text-editor-characters-expanded') ? 'expand' : 'collapse'))
+})
+$('[data-js="mathToolbar"]').on('mousedown', 'button', e => {
+    events.clickLatex++
+    hasEvents = true
+    ga('send', 'event', 'toolbar', 'latex', e.currentTarget.dataset.latexcommand)
+})
+$('[data-js="charactersList"]').on('mousedown', 'button', e => {
+    events.clickChar++
+    hasEvents = true
+    ga('send', 'event', 'toolbar', 'character', e.currentTarget.innerText)
+})
+$('[data-js="latexField"]').on('input paste', () => {
+    events.typeLatex++
+    hasEvents = true
+})
+$('[data-js="equationField"]').on('input', '.mq-textarea textarea', () => {
+    events.typeMathquill++
+    hasEvents = true
+})
+$(answer).on('mathfocus', e => {
+    if (!e.hasFocus && hasEvents) {
+        hasEvents = false
+        ga('send', 'event', 'completeMath', $('[data-js="latexField"]').val(), {
+            metric1: events.typeLatex,
+            metric2: events.typeMathquill,
+            metric3: events.clickChar,
+            metric4: events.clickLatex,
+        })
+        events.typeLatex = 0
+        events.typeMathquill = 0
+        events.clickChar = 0
+        events.clickLatex = 0
+    }
+})
