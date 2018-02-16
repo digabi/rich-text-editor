9cd86ab322f6dbb0c44d46c134bd6a0706a83360
Eero Anttila <eea@iki.fi>
2018-02-16 12:08:25 +0200

    Simplify and use custom dimension for equation

--- proto/student.front.js
+++ proto/student.front.js
@@ -29,45 +29,41 @@ if (window.addEventListener) {
 }
 const $tools = $('[data-js="tools"]')
 const events = {
-    typeLatex: 0,
-    typeMathquill: 0,
-    clickChar: 0,
-    clickLatex: 0
+    metric1: 0, //typeLatex
+    metric2: 0, //typeMathquill
+    metric3: 0, //clickChar
+    metric4: 0 //clickLatex
 }
 let hasEvents = false
 $tools.on('mousedown', '[data-js="expandCollapseCharacters"]', () => {
     ga('send', 'event', 'toolbar', 'toggle', ($tools.hasClass('rich-text-editor-characters-expanded') ? 'expand' : 'collapse'))
 })
 $('[data-js="mathToolbar"]').on('mousedown', 'button', e => {
-    events.clickLatex++
+    events.metric4++
     hasEvents = true
     ga('send', 'event', 'toolbar', 'latex', e.currentTarget.dataset.latexcommand)
 })
 $('[data-js="charactersList"]').on('mousedown', 'button', e => {
-    events.clickChar++
+    events.metric3++
     hasEvents = true
     ga('send', 'event', 'toolbar', 'character', e.currentTarget.innerText)
 })
 $('[data-js="latexField"]').on('input paste', () => {
-    events.typeLatex++
+    events.metric1++
     hasEvents = true
 })
 $('[data-js="equationField"]').on('input', '.mq-textarea textarea', () => {
-    events.typeMathquill++
+    events.metric2++
     hasEvents = true
 })
 $(answer).on('mathfocus', e => {
     if (!e.hasFocus && hasEvents) {
+        events.dimension1 = $('[data-js="latexField"]').val()
+        ga('send', 'event', 'mathEditor', 'close', events)
         hasEvents = false
-        ga('send', 'event', 'completeMath', $('[data-js="latexField"]').val(), {
-            metric1: events.typeLatex,
-            metric2: events.typeMathquill,
-            metric3: events.clickChar,
-            metric4: events.clickLatex,
-        })
-        events.typeLatex = 0
-        events.typeMathquill = 0
-        events.clickChar = 0
-        events.clickLatex = 0
+        events.metric1 = 0
+        events.metric2 = 0
+        events.metric3 = 0
+        events.metric4 = 0
     }
 })
