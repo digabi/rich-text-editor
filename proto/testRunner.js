a362a5685d48721b0251bfd6e0779f069ba88b72
Eero Anttila <eea@iki.fi>
2018-03-12 16:29:34 +0200

    Rename

--- proto/testRunner.js
+++ proto/testRunner.js
@@ -3,13 +3,13 @@
 const server = require('./server')
 const serverInfo = server.listen(0, () => {
     const spawn = require('child_process').spawn
-    const mochaPhantomjs = spawn('mocha-chrome',  [`http://localhost:${serverInfo.address().port}/tests.html`])
-    mochaPhantomjs.stdout.pipe(process.stdout)
-    mochaPhantomjs.stderr.pipe(process.stderr)
-    mochaPhantomjs.on('close', code => {
+    const mochaChrome = spawn('mocha-chrome',  [`http://localhost:${serverInfo.address().port}/tests.html`])
+    mochaChrome.stdout.pipe(process.stdout)
+    mochaChrome.stderr.pipe(process.stderr)
+    mochaChrome.on('close', code => {
         process.exit(code)
     })
-    mochaPhantomjs.on('error', err => {
+    mochaChrome.on('error', err => {
         console.log(err)
         process.exit(1)
     })
