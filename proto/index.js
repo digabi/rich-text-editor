4339ef270b4d8be2ab1163236d78ed0974a65222
Eero Anttila <eea@iki.fi>
2018-06-04 20:24:07 +0300

    Log real port

--- proto/index.js
+++ proto/index.js
@@ -3,4 +3,4 @@
 const server = require('./server')
 const interfaceIP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5000
-server.listen(port, interfaceIP, () => console.log('Server started at localhost:' + port))
+const s = server.listen(port, interfaceIP, () => console.log('Server started at :' + interfaceIP + ':' + s.address().port))
