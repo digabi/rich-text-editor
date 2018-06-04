0d602ca5329359ef556d0cdec8caf9679ffcd047
Eero Anttila <eea@iki.fi>
2018-06-04 22:35:48 +0300

    Yet another try to fix travis build

--- proto/testRunner.js
+++ proto/testRunner.js
@@ -2,16 +2,18 @@
 /* eslint-disable no-console */
 const server = require('./server')
 const serverInfo = server.listen(0, () => {
-    const spawn = require('child_process').spawn
-    const mochaChrome = spawn('mocha-chrome',  [`http://localhost:${serverInfo.address().port}/tests.html`])
-    mochaChrome.stdout.pipe(process.stdout)
-    mochaChrome.stderr.pipe(process.stderr)
-    mochaChrome.on('close', code => {
-        process.exit(code)
-    })
-    mochaChrome.on('error', err => {
-        console.log(err)
-        process.exit(1)
-    })
-    console.log('Server started at localhost:' + serverInfo.address().port)
+    setTimeout(() => {
+        console.log('Server started at localhost:' + serverInfo.address().port)
+        const spawn = require('child_process').spawn
+        const mochaChrome = spawn('mocha-chrome', [`http://localhost:${serverInfo.address().port}/tests.html`])
+        mochaChrome.stdout.pipe(process.stdout)
+        mochaChrome.stderr.pipe(process.stderr)
+        mochaChrome.on('close', code => {
+            process.exit(code)
+        })
+        mochaChrome.on('error', err => {
+            console.log(err)
+            process.exit(1)
+        })
+    }, 5000)
 })
