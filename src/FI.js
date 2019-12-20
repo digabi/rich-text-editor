8da5329942321ccc7583f621af5f5b761d2b683e
Eero Anttila <eea@iki.fi>
2019-12-20 14:45:56 +0200

    Fix translations

--- src/FI.js
+++ src/FI.js
@@ -7,7 +7,7 @@ export default {
 <li>“Lisää kaava” -napin alta löydät yleisimpiä matematiikassa, fysiikassa ja
 kemiassa käytettäviä merkintöjä. Lisäksi erikoismerkkejä voi käyttää kaavan kirjoittamiseen.</li>
  <li>Kaavoja voi rakentaa
-klikkaamalla valikon merkintöjä ja/tai kirjoittamalla LaTeXia.</li>
+napsauttamalla valikon merkintöjä ja/tai kirjoittamalla LaTeXia.</li>
  <li>Editorin vastauskenttään voi kirjoittaa tekstiä ja kaavoja sekä
 lisätä kuvia.</li></ul>`,
         shortcutTitle: 'Pikanäppäinvinkkejä',
@@ -35,7 +35,7 @@ lisätä kuvia.</li></ul>`,
         toggleInstructions: 'Näytä ohjeet',
         help_overlay: {
             screenshot:
-                '<h3>Kuvakaappaukset</h3><p>Tee kuva haluamallasi ohjelmalla. Klikkaa yläpalkista kuvakaappauskuvaketta <span class="screen-shot-image"></span> ja rajaa haluamasi kuva-alue näytöltä. <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">V</span> liittää kuvan vastausikkunaan kursorin kohdalle. Voit vaihtaa kuvan paikkaa raahaamalla tai leikkaamalla kuvan komennolla <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">X</span> ja liittämällä sen komennolla <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">V</span> haluamaasi paikkaan.</p>',
+                '<h3>Kuvakaappaukset</h3><p>Tee kuva haluamallasi ohjelmalla. Napsauta yläpalkista kuvakaappauskuvaketta <span class="screen-shot-image"></span> ja rajaa haluamasi kuva-alue näytöltä. <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">V</span> liittää kuvan vastauskenttään kursorin kohdalle. Voit vaihtaa kuvan paikkaa raahaamalla tai leikkaamalla kuvan komennolla <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">X</span> ja liittämällä sen komennolla <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">V</span> haluamaasi paikkaan.</p>',
             equation:
                 '<h3>Kaavat</h3><p>Kaava lisätään komennolla <span class="rich-text-editor-help-key">CTRL</span>-<span class="rich-text-editor-help-key">E</span><br>Pikakomennot kaavassa:</p><table class="rich-text-editor-help-shortcuts"><tbody><tr><th>Jakoviiva</th><td><span class="rich-text-editor-help-key">/</span></td></tr><tr><th>Kertomerkki</th><td><span class="rich-text-editor-help-key">*</span></td></tr><tr><th>Yläindeksi</th><td><span class="rich-text-editor-help-key">^</span></td></tr><tr><th>Alaindeksi</th><td><span class="rich-text-editor-help-key">_</span></td></tr><tr><th>Lisää kaava seuraavalle riville</th><td><span class="rich-text-editor-help-key">↵</span></td></tr><tr><th>Sulje kaava</th><td><span class="rich-text-editor-help-key">esc</span></td></tr></tbody></table>'
         }
