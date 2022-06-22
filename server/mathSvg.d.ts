6bcb65669bf7b4c1f058678d7ced0248877b1484
Eero Anttila <eea@iki.fi>
2022-06-22 09:07:44 +0300

    Add missing void return type for latexToSvg

--- server/mathSvg.d.ts
+++ server/mathSvg.d.ts
@@ -2,4 +2,4 @@ import * as express from "express";
 
 export const mathSvgResponse: express.Handler;
 
-export function latexToSvg(latex: string, cb: (svg: string) => void)
+export function latexToSvg(latex: string, cb: (svg: string) => void): void
