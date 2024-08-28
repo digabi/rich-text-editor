7d80d8dde7df1d14de69a57e9f1171ec6db74a14
Jere Nurminen <jere.nurminen@reaktor.com>
2024-08-28 16:51:03 +0300

    Use proper camelCase attribute names in SVG in React

--- src/components/ExpandIcon.tsx
+++ src/components/ExpandIcon.tsx
@@ -13,11 +13,11 @@ export const ExpandIcon = ({ style }: { style: CSSProperties }) => (
     <title>Expand</title>
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <g
         id="Kuuntelu-kokelas-painaa-nappia-Copy-18"
         transform="translate(-1069.000000, -21.000000)"
-        stroke-width="3"
+        strokeWidth="3"
         stroke="#359BB7"
       >
         <polyline id="Page-1" points="1085.99996 22 1077.99996 30 1070 22"></polyline>
