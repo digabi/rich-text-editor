28e082781641d8bcbf06c926a6324bc1974cbbe9
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2024-09-11 13:11:54 +0300

    Better doc comments

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import RichTextEditor from './rich-text-editor'
 
+// TODO: Document the portaling
+// TODO: Move to a prop
 export const TOOLBAR_ROOT = document.getElementById('toolbar')!
 export const REACT_ROOT = document.getElementById('root')!
 
