8500adcaa3c81ced3c5e1d92a2d2c01b4ad3ed92
Heikki Honkanen <heikki.honkanen@reaktor.com>
2024-10-04 13:47:24 +0300

    Prepare for building new package

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,13 +1,22 @@
 import ReactDOM from 'react-dom/client'
 import RichTextEditor from './rich-text-editor'
 
+declare global {
+  interface Window {
+    makeRichText: () => void
+  }
+}
+
 export const REACT_ROOT = document.getElementById('root')!
 
-ReactDOM.createRoot(REACT_ROOT).render(
-  <RichTextEditor
-    language="FI"
-    editorStyle={{ top: '300px', position: 'relative' }}
-    onValueChange={() => {}}
-    initialValue='testi. <br>kaava: &nbsp;<span class="math-editor-wrapper" id="math-editor-1" style="display: contents;" contenteditable="false"><img src="data:image/svg+xml;utf8," data-math-svg="true" data-latex="\sqrt{123}" alt="\sqrt{123}"></span>&nbsp;<br> <br>&nbsp;'
-  />,
-)
+export const makeRichText = () =>
+  ReactDOM.createRoot(REACT_ROOT).render(
+    <RichTextEditor
+      language="FI"
+      editorStyle={{ top: '300px', position: 'relative' }}
+      onValueChange={() => {}}
+      initialValue='testi. <br>kaava: &nbsp;<span class="math-editor-wrapper" id="math-editor-1" style="display: contents;" contenteditable="false"><img src="data:image/svg+xml;utf8," data-math-svg="true" data-latex="\sqrt{123}" alt="\sqrt{123}"></span>&nbsp;<br> <br>&nbsp;'
+    />,
+  )
+
+window.makeRichText = makeRichText
