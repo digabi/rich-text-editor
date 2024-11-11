e9a449c1b6772af9a57a653cf7bb8f871081050d
Heikki Honkanen <heikki.honkanen@reaktor.com>
2024-11-11 12:55:02 +0200

    Retry once on local machine
    Allows saving traces on failed tests when retrying,
    makes the config more similar in CI and local env.

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -13,8 +13,7 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -22,7 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-first-failure',
+    trace: 'on-first-retry',
 
     /* Port to use for Playwright component endpoint. */
     ctPort: 3100,
